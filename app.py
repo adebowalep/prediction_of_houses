12d1067939eb4b7e08be2f5b4e8ce093e6f0aa13
OJO Suleiman Adebowale <suleimanojo3@gmail.com>
2023-02-18 23:46:50 +0100

    Built a flask app

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ app = Flask(__name__)
 with open('model.pkl', 'rb') as file:
     model = pickle.load(file)
 
-cols = [ 'bedrooms', 'bathrooms', 
+cols = ['bedrooms', 'bathrooms', 'sqft_living', 'sqft_lot', 'floors',
        'waterfront', 'view', 'condition', 'grade', 'sqft_above',
-       'sqft_basement',  'lat', 
-       'sqft_living15']    
+       'sqft_basement', 'yr_built', 'yr_renovated', 'zipcode', 'lat', 'long',
+       'sqft_living15', 'sqft_lot15']
 
 @app.route('/')
 def home():
@@ -25,7 +25,15 @@ def predict():
     data_unseen = pd.DataFrame([final], columns = cols)
     prediction = model.predict(data_unseen)
     prediction = int(prediction[0])
-    return render_template('home.html',pred='Expected Bill will be {}'.format(prediction))
+    return render_template('home.html',pred='Expected price will be {}'.format(prediction))
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data = request.get_json(force=True)
+    data_unseen = pd.DataFrame([data])
+    prediction = predict(model, data=data_unseen)
+    output = prediction.Label[0]
+    return jsonify(output)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=PORT, debug=DEBUG_MODE)
